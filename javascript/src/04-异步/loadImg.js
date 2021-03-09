36e708ccca201096447584ddce47c1dd50cff7b1
周壮 <zhouzhuang95@126.com>
2021-03-09 11:15:46 +0800

    promise实现loadImg

---
+++ javascript/src/04-异步/loadImg.js
@@ -0,0 +1,12 @@
+function loadImg (src) {
+    return new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        img.onload = () => {
+            resolve(img);
+        };
+        img.onerror = () => {
+            reject(new Error(`图片加载失败 ${src}`));
+        };
+        img.src = src;
+    });
+}
