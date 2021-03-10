d3a330b3405f11c2260d9b8492e2a0d9743156b4
周壮 <zhouzhuang95@126.com>
2021-03-10 11:52:49 +0800

    防抖节流

---
+++ javascript/src/07-节流与防抖/debounce.js
@@ -0,0 +1,12 @@
+function debounce (fn, delay = 500) {
+    let timer = null;
+    return function () {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            fn.apply(this, arguments);
+            timer = null;
+        }, delay);
+    };
+}
---
+++ javascript/src/07-节流与防抖/throttle.js
@@ -0,0 +1,12 @@
+function throttle (fn, delay = 500) {
+    let timer = null;
+    return function () {
+        if (timer) {
+            return;
+        }
+        timer = setTimeout(() => {
+            fn.apply(this, arguments);
+            timer = null;
+        }, delay);
+    };
+}
